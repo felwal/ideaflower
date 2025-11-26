def8c87340b8bc3c46a9addf8bfc28ea6c4dad51
Felix Wallin <60824607+felwal@users.noreply.github.com>
2025-11-26 15:21:54 +0100

    Set up water flow sensor

--- Cloudflower/Cloudflower.ino
+++ Cloudflower/Cloudflower.ino
@@ -1,12 +1,30 @@
+#include <FlowSensor.h>; // https://github.com/hafidhh/FlowSensor-Arduino
+
+const int PIN_IN_FLOW = 2;
+
 const bool DEBUG = true;
+
+FlowSensor Sensor(700, PIN_IN_FLOW);
 
 //
 
 void setup() {
   Serial.begin(9600);
+
+  Sensor.begin(count);
+  pinMode(PIN_OUT_LED, OUTPUT);
 }
 
 void loop() {
+  Sensor.read();
+  int pulse = Sensor.getPulse();
+  float volume = Sensor.getVolume();
+  d("pulse: " + String(pulse) + "; volume: " + String(volume) + " L");
+}
+
+void count()
+{
+  Sensor.count();
 }
 
 //
