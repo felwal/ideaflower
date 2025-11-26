2eb22ef74e6827fc64cd7fe1d34978a41056986c
Felix Wallin <60824607+felwal@users.noreply.github.com>
2025-11-26 15:41:15 +0100

    Add LED output

--- Cloudflower/Cloudflower.ino
+++ Cloudflower/Cloudflower.ino
@@ -1,30 +1,70 @@
 #include <FlowSensor.h>; // https://github.com/hafidhh/FlowSensor-Arduino
 
 const int PIN_IN_FLOW = 2;
+const int PIN_OUT_LED_1 = 4;
+const int PIN_OUT_LED_2 = 5;
+const int PIN_OUT_LED_3 = 6;
 
+const float VOLUME_THRESHOLD = 0.5;
 const bool DEBUG = true;
 
-FlowSensor Sensor(700, PIN_IN_FLOW);
+FlowSensor flowSensor(700, PIN_IN_FLOW);
 
 //
 
 void setup() {
   Serial.begin(9600);
 
-  Sensor.begin(count);
-  pinMode(PIN_OUT_LED, OUTPUT);
+  flowSensor.begin(onFlowCount);
+  pinMode(PIN_OUT_LED_1, OUTPUT);
+  pinMode(PIN_OUT_LED_2, OUTPUT);
+  pinMode(PIN_OUT_LED_3, OUTPUT);
 }
 
 void loop() {
-  Sensor.read();
-  int pulse = Sensor.getPulse();
-  float volume = Sensor.getVolume();
+  flowSensor.read();
+  int pulse = flowSensor.getPulse();
+  float volume = flowSensor.getVolume();
   d("pulse: " + String(pulse) + "; volume: " + String(volume) + " L");
+
+  writeLights(volume);
+
+  if (volume >= VOLUME_THRESHOLD) {
+    //flowSensor.resetVolume();
+    d("threshold reached");
+  }
+
+  delay(500);
 }
 
-void count()
-{
-  Sensor.count();
+void onFlowCount() {
+  flowSensor.count();
+  d("flow detected");
+}
+
+void writeLights(float volume) {
+  float volumeProgress = volume / VOLUME_THRESHOLD;
+
+  if (volumeProgress < 0.33) {
+    digitalWrite(PIN_OUT_LED_1, LOW);
+    digitalWrite(PIN_OUT_LED_2, LOW);
+    digitalWrite(PIN_OUT_LED_3, LOW);
+  }
+  else if (volumeProgress < 0.66) {
+    digitalWrite(PIN_OUT_LED_1, HIGH);
+    digitalWrite(PIN_OUT_LED_2, LOW);
+    digitalWrite(PIN_OUT_LED_3, LOW);
+  }
+  else if (volumeProgress < 1.0) {
+    digitalWrite(PIN_OUT_LED_1, HIGH);
+    digitalWrite(PIN_OUT_LED_2, HIGH);
+    digitalWrite(PIN_OUT_LED_3, LOW);
+  }
+  else {
+    digitalWrite(PIN_OUT_LED_1, HIGH);
+    digitalWrite(PIN_OUT_LED_2, HIGH);
+    digitalWrite(PIN_OUT_LED_3, HIGH);
+  }
 }
 
 //
