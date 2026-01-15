74dfad9532c97cf1c2416c1109043ce8c39aa434
Felix Wallin <60824607+felwal@users.noreply.github.com>
2026-01-15 16:34:35 +0100

    Refactor plant render

--- cloudflower-web/src/views/HomeView.jsx
+++ cloudflower-web/src/views/HomeView.jsx
@@ -26,33 +26,23 @@ export default function HomeView(props) {
   }
 
   function renderIdea(idea) {
-    function renderPlant() {
-      // pot saturation and lightness
-      const sPotStart =  24;
-      const sPotEnd =  100;
-      const lPotStart =  85;
-      const lPotEnd =  50;
-      const sPot = sPotStart + (sPotEnd - sPotStart) * idea.potSaturation;
-      const lPot = lPotStart + (lPotEnd - lPotStart) * idea.potSaturation;
-
-      // leaf hue
+    function renderLeaves() {
       const hLeafStart = 53;
       const hLeafStartDk = 90;
       const hLeafEnd = 158;
       const hLeafEndDk = 195;
+
       const hLeaf = hLeafStart + (hLeafEnd - hLeafStart) * idea.leafHue;
       const hLeafDk = hLeafStartDk + (hLeafEndDk - hLeafStartDk) * idea.leafHue;
 
-      // leaf lightness
       const lLeafStart = 37;
       const lLeafStartDk = 20;
       const lLeafEnd = 47;
       const lLeafEndDk = 30;
+
       const lLeaf = lLeafStart + (lLeafEnd - lLeafStart) * idea.leafLightness;
       const lLeafDk = lLeafStartDk + (lLeafEndDk - lLeafStartDk) * idea.leafLightness;
 
-      const potColor = "hsl(from var(--color-pot) h " + sPot + " " + lPot + ")";
-      const potColorDk = "hsl(from " + potColor + " h s 35)";
       const leafColor = "hsl(from var(--color-primary-light) " + hLeaf + " s " + lLeaf + ")";
       const leafColorDk = "hsl(from var(--color-primary-dark) " + hLeafDk + " s " + lLeafDk + ")";
       const paintId = "gradient_leaf_" + idea.epoch;
@@ -62,55 +52,70 @@ export default function HomeView(props) {
         idea.leafPath = path;
       }
 
+      return (
+        <svg width="100%" height="auto" viewBox="0 0 100 100">
+          <path d={idea.leafPath} fill={"url(#" + paintId + ")"}/>
+          <defs>
+            <linearGradient id={paintId} x1="100" y1="0" x2="0" y2="100" gradientUnits="userSpaceOnUse">
+              <stop stop-color={leafColor}/>
+              <stop offset="1" stop-color={leafColorDk}/>
+            </linearGradient>
+          </defs>
+        </svg>
+      );
+    }
+
+    function renderSeed() {
       const waterProgress = useFlowStore().waterLevel / 8.0;
       const yWaterLevel = Math.round(waterProgress * 27);
       const yWaterHeight = 27 - yWaterLevel;
 
       return (
-        <div class="plant">
-          <div class={"plant__leaves"}>
-            {
-              idea.result ?
-                <svg width="100%" height="auto" viewBox="0 0 100 100">
-                  <path d={idea.leafPath} fill={"url(#" + paintId + ")"}/>
-                  <defs>
-                    <linearGradient id={paintId} x1="100" y1="0" x2="0" y2="100" gradientUnits="userSpaceOnUse">
-                      <stop stop-color={leafColor}/>
-                      <stop offset="1" stop-color={leafColorDk}/>
-                    </linearGradient>
-                  </defs>
-                </svg> :
-                <svg width="100%" height="auto" viewBox="0 0 46 46" fill="none">
-                  <defs>
-                    <mask id="mask_water_level">
-                      <rect x="24" width="22" height={yWaterLevel} fill="white" fill-opacity="var(--alpha-water-empty)"/>
-                      <rect x="24" y={yWaterLevel} width="22" height={yWaterHeight} fill="white" fill-opacity="1"/>
-                    </mask>
-                  </defs>
-                  <circle cx="23" cy="35" r="6" fill="var(--color-seed)"/>
-                  <path d="M42.7782 23.3345C47.0739 19.0388 47.0739 12.0739 42.7782 7.77817L35 0L27.2218 7.77817C22.9261 12.0739 22.9261 19.0388 27.2218 23.3345C31.5176 27.6303 38.4824 27.6303 42.7782 23.3345Z" fill="var(--color-water)" mask="url(#mask_water_level)"/>
-                </svg>
-            }
-          </div>
-          <div class="plant__pot__back">
-            <svg width="100%" height="auto" viewBox="0 0 46 3" fill="none">
-              <path d="M23 0C35.7025 0 46 1.34315 46 3H0C0 1.34315 10.2975 0 23 0Z" fill={potColorDk}/>
-            </svg>
-          </div>
-          <div class="plant__pot__front">
-            <svg width="100%" height="auto" viewBox="0 0 46 27" fill="none">
-              <ellipse cx="23" cy="24" rx={idea.potWide ? "19" : "13"} ry="3" fill={potColor}/>
-              <path d={idea.potWide ? "M0 0H46L42 24H4L0 0Z" : "M0 0H46L36 24H10L0 0Z"} fill={potColor}/>
-              <path d="M46 0C46 1.65685 35.7025 3 23 3C10.2975 3 0 1.65685 0 0H46Z" fill={potColorDk}/>
-            </svg>
-          </div>
+        <svg width="100%" height="auto" viewBox="0 0 46 46" fill="none">
+          <defs>
+            <mask id="mask_water_level">
+              <rect x="24" width="22" height={yWaterLevel} fill="white" fill-opacity="var(--alpha-water-empty)"/>
+              <rect x="24" y={yWaterLevel} width="22" height={yWaterHeight} fill="white" fill-opacity="1"/>
+            </mask>
+          </defs>
+          <circle cx="23" cy="35" r="6" fill="var(--color-seed)"/>
+          <path d="M42.7782 23.3345C47.0739 19.0388 47.0739 12.0739 42.7782 7.77817L35 0L27.2218 7.77817C22.9261 12.0739 22.9261 19.0388 27.2218 23.3345C31.5176 27.6303 38.4824 27.6303 42.7782 23.3345Z" fill="var(--color-water)" mask="url(#mask_water_level)"/>
+        </svg>
+      );
+    }
+
+    function renderPot() {
+      const sPotStart =  24;
+      const sPotEnd =  100;
+      const lPotStart =  85;
+      const lPotEnd =  50;
+
+      const sPot = sPotStart + (sPotEnd - sPotStart) * idea.potSaturation;
+      const lPot = lPotStart + (lPotEnd - lPotStart) * idea.potSaturation;
+
+      const potColor = "hsl(from var(--color-pot) h " + sPot + " " + lPot + ")";
+      const potColorDk = "hsl(from " + potColor + " h s 35)";
+
+      return (
+        <div class="plant__pot">
+          <svg class="plant__pot__back"width="100%" height="auto" viewBox="0 0 46 3" fill="none">
+            <path d="M23 0C35.7025 0 46 1.34315 46 3H0C0 1.34315 10.2975 0 23 0Z" fill={potColorDk}/>
+          </svg>
+          <svg class="plant__pot__front" width="100%" height="auto" viewBox="0 0 46 27" fill="none">
+            <ellipse cx="23" cy="24" rx={idea.potWide ? "19" : "13"} ry="3" fill={potColor}/>
+            <path d={idea.potWide ? "M0 0H46L42 24H4L0 0Z" : "M0 0H46L36 24H10L0 0Z"} fill={potColor}/>
+            <path d="M46 0C46 1.65685 35.7025 3 23 3C10.2975 3 0 1.65685 0 0H46Z" fill={potColorDk}/>
+          </svg>
         </div>
       );
     }
 
     return (
       <div class="item">
-        {renderPlant()}
+        <div class="plant">
+          <div class="plant__leaves">{idea.result ? renderLeaves() : renderSeed()}</div>
+          {renderPot()}
+        </div>
         <h3 class="item__name">{idea.name || "???"}</h3>
         <p class="item__date">{new Date(idea.epoch).toLocaleString()}</p>
       </div>
