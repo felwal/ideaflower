b5cb0f45115e5ae817e188413b7885cb4c3a5b8e
Felix Wallin <60824607+felwal@users.noreply.github.com>
2026-01-22 14:18:17 +0100

    Use hash history

--- ideagarden/src/router/index.js
+++ ideagarden/src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomePresenter from "@/presenters/HomePresenter";
 import ProfilePresenter from "@/presenters/ProfilePresenter";
 import IdeaPresenter from "@/presenters/IdeaPresenter";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
